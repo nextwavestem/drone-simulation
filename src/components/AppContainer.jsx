c3d5326ebd06c84c0e9151fb3d2d3408f927848a
nextwavestem <tanknology.nws@gmail.com>
2024-11-07 09:28:37 -0600

    update cookie andlogin check

--- src/components/AppContainer.jsx
+++ src/components/AppContainer.jsx
@@ -1,13 +1,26 @@
-import '../css/appContainer.css'
+import '../css/appContainer.css';
 import PropTypes from 'prop-types';
 import { useNavigate } from 'react-router-dom';
-import {getBannerReference} from '../config/navigationConfig.js'
+import { useEffect } from 'react';
+import { getBannerReference } from '../config/navigationConfig.js';
 
 export const AppContainer = ({ children }) => {
     const navigate = useNavigate();
 
     const handleLogoClick = () => { navigate('/'); };
-    
+
+    useEffect(() => {
+        const getCookieValue = (name) => {
+            const match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
+            return match ? decodeURIComponent(match[2]) : null;
+        };
+
+        const sessionActive = getCookieValue('session_active'); 
+        if (!sessionActive) {
+            navigate('/');
+        }
+    }, [navigate]);
+
     return (
         <div className="application-container"> 
             <div className="header-container">
@@ -19,8 +32,8 @@ export const AppContainer = ({ children }) => {
                 { children }
             </div>
         </div>
-    )
-}
+    );
+};
 
 AppContainer.propTypes = {
     children: PropTypes.node,
