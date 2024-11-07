8dd33f1ff473c12d94327a099f339d42ce4bebb0
nextwavestem <tanknology.nws@gmail.com>
2024-11-06 22:47:09 -0600

    update images

--- src/home/dashboard/config.js
+++ src/home/dashboard/config.js
@@ -21,7 +21,7 @@ export const projects = () => {
             id: 2, 
             link: "/city/",
             title: "City", 
-            image: getImagePrefix("fixtures/city_2.png"), 
+            image: getImagePrefix("fixtures/c1.png"), 
             detail: "Learn how to navigate your drone in a city using blockly",
             description1: "In the simulator, users take control of a drone, learning essential flight actions such as takeoff, landing, and maneuvering in three-dimensional space. As they explore, children can complete tasks that encourage critical thinking and spatial awareness, all while gaining practical knowledge about drone operation and safety.",
             description2: "The City Drone Simulator not only fosters creativity and curiosity but also introduces children to the principles of aviation and technology in an engaging way. It’s a unique blend of play and education, making learning about drones both enjoyable and accessible. With its user-friendly interface and captivating city environment, this simulator is the perfect platform for young adventurers to take flight!"
@@ -30,7 +30,7 @@ export const projects = () => {
             id: 3, 
             link: "/egypt/",
             title: "Egypt", 
-            image: getImagePrefix("fixtures/egypt.jpg"), 
+            image: getImagePrefix("fixtures/e1.png"), 
             detail: "Learn how to navigate your drone in a Egypt using blockly",
             description1:"In the simulator, users take control of a drone, learning essential flight actions such as takeoff, landing, and maneuvering in three-dimensional space. As they explore a vibrant Egyptian landscape filled with iconic landmarks like the Pyramids of Giza and the Nile River, children can complete tasks that encourage critical thinking and spatial awareness, all while gaining practical knowledge about drone operation and safety.",
             description2:"The Egypt Drone Simulator not only fosters creativity and curiosity but also introduces children to the principles of aviation and technology in an engaging way. Set against the backdrop of ancient monuments and bustling markets, this simulator provides a unique blend of play and education, making learning about drones both enjoyable and accessible. With its user-friendly interface and captivating Egyptian environment, this simulator is the perfect platform for young adventurers to take flight!"
@@ -39,7 +39,7 @@ export const projects = () => {
             id: 4, 
             link: "/himalayas/",
             title: "Himalayas", 
-            image: getImagePrefix("fixtures/himalayas.jpg"), 
+            image: getImagePrefix("fixtures/m1.png"), 
             detail: "Learn how to navigate your drone in a Himalayas using blockly",
             description1: "In the simulator, users take control of a drone, learning essential flight actions such as takeoff, landing, and maneuvering in three-dimensional space. As they explore majestic mountain ranges with soaring peaks, lush valleys, and winding trails, children can complete tasks that enhance critical thinking and spatial awareness, all while gaining practical knowledge about drone operation and safety.",
             description2: "The Mountain Range Drone Simulator fosters creativity and curiosity by introducing children to the principles of aviation and technology in a breathtaking natural environment. Set against rugged cliffs, scenic rivers, and vast forests, this simulator provides an engaging blend of play and education, making learning about drones both enjoyable and accessible. With its user-friendly interface and stunning mountain landscapes, this simulator is the perfect platform for young adventurers to take flight and discover the thrill of exploration!"
@@ -48,7 +48,7 @@ export const projects = () => {
             id: 5, 
             link: "/slate/",
             title: "Slate", 
-            image: getImagePrefix("fixtures/slate.jpg"), 
+            image: getImagePrefix("fixtures/s1.png"), 
             detail: "Learn how to navigate your drone to draw letters using blockly",
             description1:"Unlock your creativity as you learn to navigate your drone and draw letters in this environment using Blockly, a visual programming language designed for all ages! In this engaging activity, you'll explore the fundamentals of drone programming while gaining hands-on experience in controlling your drone's movements.",
         },
