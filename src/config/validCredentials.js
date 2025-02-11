14084998afc046c81312f09df2a1e3866fa6c5e8
tankankita <tankankita02@gmail.com>
2025-02-11 11:08:49 -0600

    update password check for test user

--- src/config/validCredentials.js
+++ src/config/validCredentials.js
@@ -4,5 +4,8 @@ export const validCredentials = [
     },
     {
       accessCode: `${new Date().toLocaleString("default",{month:"long"})}${new Date().getFullYear()}`,
+    },
+    {
+      accessCode: 'testUser',
     }
 ];
