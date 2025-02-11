8d5072a25f672cd565ab0ef419f0b77f57db7532
haritaanbuvelan <haritavelan@gmail.com>
2025-02-11 10:59:09 -0600

    password update

--- src/config/validCredentials.js
+++ src/config/validCredentials.js
@@ -1,5 +1,8 @@
 export const validCredentials = [
     {
       accessCode: 'December2024',
+    },
+    {
+      accessCode: `${new Date().toLocaleString("default",{month:"long"})}${new Date().getFullYear()}`,
     }
 ];
